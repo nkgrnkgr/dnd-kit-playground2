7202031ce4bc4d2f10d74602b911e75efc4484f4
taiki-nishi <taiki-nishi@cybozu.co.jp>
2023-02-24 09:36:22 +0900

    delete recoil

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { RecoilRoot } from "recoil";
 import { App } from "./App";
 import { createRootStore } from "./modules/store";
 
@@ -10,9 +9,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={createRootStore()}>
       <ChakraProvider>
-        <RecoilRoot>
-          <App />
-        </RecoilRoot>
+        <App />
       </ChakraProvider>
     </Provider>
   </React.StrictMode>
